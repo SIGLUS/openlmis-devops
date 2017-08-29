685b939ff4457cf0e25ee0e0eef82ff26cc33d22
Miguel Rueda <mrueda@thoughtworks.com>
2017-08-29 11:09:05 -0500

    US00 - MR - remove db-migration from devops repo as shouldn't be part of this codebase

--- deployment/db-migration/migration/V12__update_VIA_name.sql
+++
@@ -1,1 +0,0 @@
-UPDATE programs SET name = 'VIA Classica' WHERE code = 'VIA';
--- deployment/db-migration/migration/V13__add_national_facility_type.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO facility_types (code, name, nominalMaxMonth, nominalEop, active) VALUES
-  ('Central', 'Moçambique', 3, 0.5, TRUE);
