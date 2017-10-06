d0323c2e413167a4ba0fa5f57a8e68cf7a34cfdc
SamMolina <smolina@thoughtworks.com>
2017-10-06 17:00:56 -0500

    TT67 - FC/SM - Add gocd string encryptor file

---
+++ portal_deployment/scripts/gocd_string_encryptor.sh
@@ -0,0 +1,11 @@
+read -p "Enter user name: " username
+echo "Enter password: "
+read -s pass
+read -p "Enter the string to encrypt: " string
+curl 'https://52.69.16.156:8154/go/api/admin/encrypt' \
+-u "$username:$pass" \
+-H 'Accept: application/vnd.go.cd.v1+json' \
+-H 'Content-Type: application/json' \
+-X POST -d '{
+  "value": "$string"
+}' --insecure
