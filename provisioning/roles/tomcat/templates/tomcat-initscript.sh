480059902189b4ac1c60f6b5f5906ee35a1595dd
faustocv <fausto.castaneda.v@gmail.com>
2017-10-19 08:48:21 -0500

    TT67 - MR/FC configures tomcat to use openjdk 7

--- provisioning/roles/tomcat/templates/tomcat-initscript.sh
+++ provisioning/roles/tomcat/templates/tomcat-initscript.sh
@@ -15,7 +15,7 @@
 #
 
 #Location of JAVA_HOME (bin files)
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64/
 
 #Add Java binary files to PATH
 export PATH=$JAVA_HOME/bin:$PATH
