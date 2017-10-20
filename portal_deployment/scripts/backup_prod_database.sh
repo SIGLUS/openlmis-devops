34d639dd73d5358c3fccb29055f4a5edbcde91b7
faustocv <fausto.castaneda.v@gmail.com>
2017-10-20 17:01:45 -0500

    TT67 - MR/FC Fixes script again

--- portal_deployment/scripts/backup_prod_database.sh
+++ portal_deployment/scripts/backup_prod_database.sh
@@ -2,4 +2,4 @@
 
 set -e
 DATE=`date +%Y-%m-%d-%H-%M`
-docker run --rm --volumes-from $HOSTNAME -w /godata/pipelines/portal_deployment/portal_deployment/scripts/ siglus/taskrunner:0.0.2 sh -c "mkdir -p backups && PGPASSWORD=$DBPASSWORD pg_dump -h 172.31.4.20-U $DBUSER -w -Fc -f backups/openlmis-backup-$DATE $DBNAME"
+docker run --rm --volumes-from $HOSTNAME -w /godata/pipelines/portal_deployment/portal_deployment/scripts/ siglus/taskrunner:0.0.2 sh -c "mkdir -p backups && PGPASSWORD=$DBPASSWORD pg_dump -h 172.31.4.20 -U $DBUSER -w -Fc -f backups/openlmis-backup-$DATE $DBNAME"
