e9a781810e35956c8b45cac37fda9e325d2b7bee
faustocv <fausto.castaneda.v@gmail.com>
2017-10-20 16:17:59 -0500

    TT67 - MR/FC Fixes backup database script using docker

--- portal_deployment/scripts/backup_prod_database.sh
+++ portal_deployment/scripts/backup_prod_database.sh
@@ -2,4 +2,4 @@
 
 set -e
 DATE=`date +%Y-%m-%d-%H-%M`
-pg_dump -U $DBUSER -Fc -f openlmis-backup-$DATE $DBNAME -h 172.31.4.20 -p $DBPASSWORD
+docker run --rm siglus/taskrunner:0.0.2 sh -c "pg_dump -U $DBUSER -Fc -f openlmis-backup-$DATE $DBNAME -h 172.31.4.20 -p $DBPASSWORD"
