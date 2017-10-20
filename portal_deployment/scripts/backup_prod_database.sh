af309e5776008e04123f34526d2d27f0128db237
faustocv <fausto.castaneda.v@gmail.com>
2017-10-20 16:35:48 -0500

    TT67 - MR/FC Adds volumes to the backup prod database script

--- portal_deployment/scripts/backup_prod_database.sh
+++ portal_deployment/scripts/backup_prod_database.sh
@@ -2,4 +2,4 @@
 
 set -e
 DATE=`date +%Y-%m-%d-%H-%M`
-docker run --rm siglus/taskrunner:0.0.2 sh -c "pg_dump -U $DBUSER -Fc -f openlmis-backup-$DATE $DBNAME -h 172.31.4.20 -p $DBPASSWORD"
+docker run --rm --volumes-from $HOSTNAME -w /godata/pipelines/portal_deployment/portal_deployment/scripts/ siglus/taskrunner:0.0.2 sh -c "mkdir -p backups && pg_dump -U $DBUSER -Fc -f backups/openlmis-backup-$DATE $DBNAME -h 172.31.4.20 -p $DBPASSWORD"
