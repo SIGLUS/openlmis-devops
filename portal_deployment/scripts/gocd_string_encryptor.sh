6e7e05ce7ef8603ad4d3c2ec504d3dc1beeb9add
faustocv <fausto.castaneda.v@gmail.com>
2017-10-26 16:12:04 -0500

    TT69 - MR/FC Updates encryptor script

--- portal_deployment/scripts/gocd_string_encryptor.sh
+++ portal_deployment/scripts/gocd_string_encryptor.sh
@@ -3,7 +3,7 @@ read -p "Enter user name: " username
 echo "Enter password: "
 read -s pass
 read -p "Enter the string to encrypt: " string
-curl 'https://13.112.130.59:8154/go/api/admin/encrypt' \
+curl 'https://52.193.130.59:8154/go/api/admin/encrypt' \
 -u "$username:$pass" \
 -H 'Accept: application/vnd.go.cd.v1+json' \
 -H 'Content-Type: application/json' \
