6c5b6f519d113429fc3242a3568f5863de00ed9a
focus.li <hongyao_li@126.com>
2018-06-15 00:34:40 -0500

    Update backup_prod_database.sh

--- portal_deployment/scripts/backup_prod_database.sh
+++ portal_deployment/scripts/backup_prod_database.sh
@@ -2,4 +2,4 @@
 
 set -e
 DATE=`date +%Y-%m-%d-%H-%M`
-docker run --rm --volumes-from $HOSTNAME -w /godata/pipelines/portal_deployment/portal_deployment/scripts/ siglus/taskrunner:0.0.2 sh -c "mkdir -p backups && PGPASSWORD=$DBPASSWORD pg_dump -h 172.31.4.20 -U $DBUSER -f backups/openlmis-backup-$DATE.sql open_lmis && cd backups && tar -czvf openlmis-backup-$DATE.sql.gz openlmis-backup-$DATE.sql && rm openlmis-backup-$DATE.sql"
+docker run --rm --volumes-from $HOSTNAME -w /godata/pipelines/portal_deployment/portal_deployment/scripts/ siglus/taskrunner:0.0.2 sh -c "mkdir -p backups && PGPASSWORD=$DBPASSWORD pg_dump -h 54.251.142.73 -U $DBUSER -f backups/openlmis-backup-$DATE.sql open_lmis && cd backups && tar -czvf openlmis-backup-$DATE.sql.gz openlmis-backup-$DATE.sql && rm openlmis-backup-$DATE.sql"
