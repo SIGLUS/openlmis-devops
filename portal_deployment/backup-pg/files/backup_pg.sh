6a8465504749f3adf281f1f15dc37bb301c9ee27
peizhao11 <peizhao@thoughtworks.com>
2018-08-20 10:03:00 +0800

    change the pgdara backup dictory

--- portal_deployment/backup-pg/files/backup_pg.sh
+++ portal_deployment/backup-pg/files/backup_pg.sh
@@ -4,11 +4,11 @@
 
 now=$(date +%Y%m%d_%H%M%S)
 
-pg_dump open_lmis -U openlmis > /pgdata/backup/openlmis-$now.sql 
+pg_dump open_lmis -U openlmis > /data/pgdata_bak/openlmis-$now.sql 
 
-cd /pgdata/backup
+cd /data/pgdata_bak
 tar czf  openlmis-$now.sql.tar.gz openlmis-$now.sql 
 rm openlmis-$now.sql
 
-cd /pgdata/backup
+cd /data/pgdata_bak
 ls -lt | awk '{if(NR>=6){print $9}}' | xargs rm -f
